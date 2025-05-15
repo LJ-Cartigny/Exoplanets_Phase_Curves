21ba328f931320db5b96799f60ec9084b6712048
ufe local user <localuser@e-m1-fr-15.obspm.fr>
2025-05-15 10:39:17 +0200

    Added computation of the star flux with the SPHINX spectrum and MIRI filters

--- Phase_curve_wavelength.py
+++ Phase_curve_wavelength.py
@@ -8,7 +8,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
+from scipy.interpolate import interp1d
 from scipy.constants import c, h, k, sigma
+from tqdm import tqdm
+import time
 
 from Orbital_motion import compute_true_anomaly
 from Transits import eclipse
@@ -178,7 +181,7 @@ def conversion_mJy_to_IS(F_mJy, wavelength, dist, R):
     :param F_mJy: the flux density (in mJy)
     :type F_mJy: float
 
-    :param wavelength: the wavelength (in m)
+    :param wavelength: the wavelength (in m)flux_T1_sphinx_cut *= QE
     :type wavelength: float
 
     :param dist: the distance of the object (in m)
@@ -223,6 +226,182 @@ def flux_Wm2(F_mJy, lambda_min, lambda_max, dist, R):
 
     return F
 
+def filter(filter_name):
+    """
+    Returns the filter band of the specified filter.
+
+    :param filter_name: the name of the filter
+    :type filter_name: str
+
+    :return: filter_band
+    :rtype: np.ndarray
+    """
+
+    if filter_name == "F560W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 1))
+
+    elif filter_name == "F770W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 2))
+
+    elif filter_name == "F1000W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 3))
+
+    elif filter_name == "F1130W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 4))
+
+    elif filter_name == "F1280W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 5))
+
+    elif filter_name == "F1500W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 6))
+
+    elif filter_name == "F1800W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 7))
+
+    elif filter_name == "F2100W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 8))
+
+    elif filter_name == "F2550W":
+        filter_band = np.loadtxt("miri_filter.csv", delimiter=',', unpack=True, skiprows=1, usecols=(0, 9))
+
+
+    return filter_band
+
+
+def quantum_efficiency(filter_name, wavelength):
+    """
+    Returns the quantum efficiency of the specified filter at the given wavelength.
+
+    :param filter_name: the name of the filter
+    :type filter_name: str
+
+    :param wavelength: the wavelength (in m)
+    :type wavelength: float
+
+    :return: QE
+    :rtype: float
+    """
+
+    filter_band = filter(filter_name)
+    wavelengths = filter_band[0,:]*1e-6 # Convert to m
+
+    # lambda_min = np.min(wavelengths)
+    # lambda_max = np.max(wavelengths)
+
+    # i = 0
+    # while wavelengths_T1_sphinx[i] < lambda_min:
+    #     i += 1
+    # i_min = i
+    # while wavelengths_T1_sphinx[i] < lambda_max:
+    #     i += 1
+    # i_max = i
+
+    # wavelengths_T1_sphinx_cut = wavelengths_T1_sphinx[i_min:i_max]
+
+    # QE = np.interp(wavelength, wavelengths, filter_band[1,:]/np.max(filter_band[1,:]))
+
+    QE = np.interp(wavelength, wavelengths, filter_band[1,:])
+
+    # QE = np.interp(wavelengths_T1_sphinx_cut, wavelengths, filter_band[1,:])
+
+    return QE
+
+
+def flux_star_miri(filter_name):
+    """
+    Returns the flux of the star in the specified filter band.
+
+    :param filter_name: the name of the filter
+    :type filter_name: str
+
+    :return: F_star
+    :rtype: float
+    """
+
+    filter_band = filter(filter_name)
+    wavelengths = filter_band[0,:]*1e-6 # Convert to m
+
+    # print(np.min(wavelengths), np.max(wavelengths))
+    # print(wavelengths.shape)
+
+    # print(np.min(wavelengths_T1_sphinx), np.max(wavelengths_T1_sphinx))
+    # print(wavelengths_T1_sphinx.shape)
+
+    lambda_min = np.min(wavelengths)
+    lambda_max = np.max(wavelengths)
+
+    i = 0
+    while wavelengths_T1_sphinx[i] < lambda_min:
+        i += 1
+    i_min = i
+    while wavelengths_T1_sphinx[i] < lambda_max:
+        i += 1
+    i_max = i
+
+    flux_T1_sphinx_cut = flux_T1_sphinx[i_min:i_max]
+    wavelengths_T1_sphinx_cut = wavelengths_T1_sphinx[i_min:i_max]
+    flux_interp = interp1d(wavelengths_T1_sphinx_cut, flux_T1_sphinx_cut, kind='linear', bounds_error=False, fill_value=0)
+
+    # mid_point = (lambda_min + lambda_max) / 2
+    # F_star_1 = quad(lambda l: quantum_efficiency(filter_name, l) * flux_interp(l), lambda_min, mid_point)[0]
+    # F_star_2 = quad(lambda l: quantum_efficiency(filter_name, l) * flux_interp(l), mid_point, lambda_max)[0]
+    # F_star = F_star_1 + F_star_2
+
+    # print(flux_T1_sphinx.shape)
+
+    # flux_T1_sphinx_interp = np.interp(wavelengths, wavelengths_T1_sphinx, flux_T1_sphinx)
+
+    # print(wavelengths.shape)
+    # print(flux_T1_sphinx_interp.shape)
+
+    # F_star = quad(lambda l: quantum_efficiency(filter_name, l)*flux_interp(l), lambda_min, lambda_max, limit=100)[0]
+
+    # F_star = quad(lambda l: Planck_law(l, T_eff_star)*quantum_efficiency(filter_name, l), wavelengths[0], wavelengths[-1])[0]
+    # F_star = np.trapz(flux_T1_sphinx_interp*quantum_efficiency(filter_name,wavelengths), wavelengths)
+    
+    F_star = 0
+    for l in tqdm(wavelengths_T1_sphinx_cut):
+        time.sleep(0.1)
+        QE = quantum_efficiency(filter_name, l)
+        F_star += flux_interp(l) * QE
+
+    F_star *= (wavelengths_T1_sphinx_cut[1]-wavelengths_T1_sphinx_cut[0])
+
+    # F_star = np.sum(flux_T1_sphinx_cut*quantum_efficiency(filter_name))*(wavelengths_T1_sphinx[1]-wavelengths_T1_sphinx[0])
+
+    return F_star
+
+def flux_miri2(filter_name):
+    """
+    Returns the flux of the star in the specified filter band.
+
+    :param filter_name: the name of the filter
+    :type filter_name: str
+
+    :return: F_star
+    :rtype: float
+    """
+
+    filter_band = filter(filter_name)
+    wavelengths_filter = filter_band[0,:]*1e-6 # Convert to m
+    QE = filter_band[1,:]/np.max(filter_band[1,:])
+    
+    # The two arrays are not covering the same wevelength range so we need to deterùine the common range
+    lambda_min_common = np.max([np.min(wavelengths_T1_sphinx), np.min(wavelengths_filter)])
+    lambda_max_common = np.min([np.max(wavelengths_T1_sphinx), np.max(wavelengths_filter)])
+
+    mask_common = (wavelengths_T1_sphinx >= lambda_min_common) & (wavelengths_T1_sphinx <= lambda_max_common)
+    wavelengths_T1_sphinx_cut = wavelengths_T1_sphinx[mask_common]
+    flux_T1_sphinx_cut = flux_T1_sphinx[mask_common]
+
+    # Interpolate the QE values to match the wavelengths of the SPHINX spectrum
+    interp_filter = interp1d(wavelengths_filter, QE, bounds_error=False, fill_value=0)
+    QE_interp = interp_filter(wavelengths_T1_sphinx_cut)
+
+    F_star = np.trapz(flux_T1_sphinx_cut * QE_interp, wavelengths_T1_sphinx_cut)
+
+    return F_star
+
 
 def main():
 
@@ -230,25 +409,28 @@ def main():
 
     # Comparison of the SPHINX and black body spectra of TRAPPIST-1
 
-    flux_Planck = Planck_law(wavelengths_T1_sphinx, T_eff_star)
+    # flux_Planck = Planck_law(wavelengths_T1_sphinx, T_eff_star)
 
-    plt.figure(figsize=(16,9))
-    plt.plot(wavelengths_T1_sphinx, flux_T1_sphinx, label="SPHINX")
-    plt.plot(wavelengths_T1_sphinx, flux_Planck, label="Black body")
-    plt.xlabel(r"Wavelength ($m$)")
-    plt.ylabel(r"Flux ($W/m^2/m$)")
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.xlim(np.min(wavelengths_T1_sphinx), np.max(wavelengths_T1_sphinx))
-    plt.title("Comparison of the SPHINX and black body spectra of TRAPPIST-1")
-    plt.legend()
-    plt.grid()
-    plt.show()
+    # plt.figure(figsize=(16,9))
+    # plt.plot(wavelengths_T1_sphinx, flux_T1_sphinx, label="SPHINX")
+    # plt.plot(wavelengths_T1_sphinx, flux_Planck, label="Black body")
+    # plt.xlabel(r"Wavelength ($m$)")
+    # plt.ylabel(r"Flux ($W/m^2/m$)")
+    # plt.xscale("log")
+    # plt.yscale("log")
+    # plt.xlim(np.min(wavelengths_T1_sphinx), np.max(wavelengths_T1_sphinx))
+    # plt.title("Comparison of the SPHINX and black body spectra of TRAPPIST-1")
+    # plt.legend()
+    # plt.grid()
+    # plt.show()
     
     # Define MIRI 15 micron filter band
 
     lambda_min_F1500 = 13e-6 # in m
     lambda_max_F1500 = 17.3e-6 # in m
+
+    # QE = quantum_efficiency("F1500W", 15e-6)
+    # print("QE = ", QE)
 
     #wavelengths = np.linspace(lambda_min, lambda_max, nb_points)
 
@@ -265,6 +447,12 @@ def main():
     #print("F_star_obs = ", F_star_obs_Wm2m, "W/m^2/m (seen from Earth)")
     F_star_obs_Wm2 = flux_Wm2(F_star_obs_mJy, lambda_min_F1500, lambda_max_F1500, dist_system, R_star)
     print("F_star_obs = ", F_star_obs_Wm2, "W/m^2 (seen from Earth)")
+
+    # F_star_sphinx = flux_star_miri("F1500W")
+    # print("F_star_sphinx = ", F_star_sphinx, "W/m^2")
+
+    F_star_sphinx = flux_miri2("F1500W")
+    print("F_star_sphinx = ", F_star_sphinx, "W/m^2")
 
 
     # For TRAPPIST-1 b
